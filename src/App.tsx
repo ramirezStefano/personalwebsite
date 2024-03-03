4221c1e640f94ef28c0c4ab4396673ea7c558097
Stefano Ramirez <ramirez.stefano@gmail.com>
2024-03-03 12:01:42 -0600

    add page footer and landing content

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import "./App.css";
-
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import { Outlet } from "react-router-dom";
+import LandingPage from "./components/LandingPage/LandingPage";
 
 function App() {
   return (
     <>
       <Header />
+      <LandingPage />
       <Outlet />
+      <Footer />
     </>
   );
 }
