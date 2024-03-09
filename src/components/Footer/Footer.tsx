e70a176dc65adcf9f38df5252102d512ac0883fd
Stefano Ramirez <ramirez.stefano@gmail.com>
2024-03-09 12:57:53 -0600

    changes to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,15 +1,29 @@
 import { FC } from "react";
-import { Typography, Grid } from "@mui/material";
+import { Typography, Grid, useTheme } from "@mui/material";
 interface FooterProps {}
 
 const Footer: FC<FooterProps> = () => {
+  const theme = useTheme();
+
   return (
-    <Grid sx={{ position: "fixed", bottom: 0, width: "100vw" }}>
-      <Typography variant="body1">
+    <Grid
+      sx={{
+        position: "fixed",
+        bottom: 0,
+        width: "100vw",
+        paddingTop: 1,
+        backgroundColor: theme.palette.secondary.main,
+      }}
+    >
+      <Typography variant="body1" color={theme.palette.text.secondary}>
         © [2024] ramirezstefano.com All rights reserved.
       </Typography>
 
-      <Typography variant="body1" paragraph>
+      <Typography
+        variant="body1"
+        color={theme.palette.text.secondary}
+        paragraph
+      >
         6-26 Campo Real, San Rafael Alajuela, San Jose, Costa Rica
       </Typography>
     </Grid>
