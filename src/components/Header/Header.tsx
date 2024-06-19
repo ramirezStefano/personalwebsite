fb498c391df086e3663e7f5da58d704664d26fe6
Stefano Ramirez <ramirez.stefano@gmail.com>
2024-06-19 16:16:27 -0600

    change nav for mobile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,13 +2,9 @@ import { FC, useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
-// import MenuItem from "@mui/material/MenuItem";
 import { Link, useNavigate } from "react-router-dom";
 
 const pages = ["About", "Education", "Experience", "Skills", "Portfolio"];
@@ -57,7 +53,7 @@ const Header: FC = () => {
             component="a"
             sx={{
               mr: 2,
-              display: { xs: "none", md: "flex" },
+              display: { xs: "none", md: "grid" },
               fontFamily: "monospace",
               fontWeight: 700,
               letterSpacing: ".3rem",
@@ -67,54 +63,12 @@ const Header: FC = () => {
           >
             <Link to={`/`}>RAMIREZ STEFANO</Link>
           </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleTopNavButtons}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {pages.map((page) => (
-                <IconButton
-                  key={page}
-                  onClick={handleTopNavButtons}
-                  onMouseDown={handleTopNavButtons}
-                >
-                  {page}
-                </IconButton>
-              ))}
-            </Menu>
-          </Box>
           <Typography
             variant="h5"
-            noWrap
             component="a"
             sx={{
               mr: 2,
-              display: { xs: "flex", md: "none" },
+              display: { xs: "grid", md: "none" },
               flexGrow: 1,
               fontFamily: "monospace",
               fontWeight: 700,
@@ -125,7 +79,7 @@ const Header: FC = () => {
           >
             <Link to={`/`}>RAMIREZ STEFANO</Link>
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
               <Button
                 key={page}
