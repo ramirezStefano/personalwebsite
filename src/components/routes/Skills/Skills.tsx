30a492432837451cc22e6a63c7c60610f966e38f
Stefano Ramirez <ramirez.stefano@gmail.com>
2024-06-20 01:29:34 -0600

    add: animations to different elements

--- src/components/routes/Skills/Skills.tsx
+++ src/components/routes/Skills/Skills.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { Grid, Paper, Typography } from "@mui/material";
+import "../../../App.css";
 interface SkillsProps {}
 
 const Skills: FC<SkillsProps> = () => {
@@ -11,22 +12,25 @@ const Skills: FC<SkillsProps> = () => {
         sx={{ padding: 2, marginTop: 12 }}
       >
         <Grid item xs={12} sm={8}>
-          <Paper sx={{ padding: 2, marginBottom: 4 }}>
-            <Typography variant="h4" gutterBottom>
+          <Paper
+            className={"animate delay-1"}
+            sx={{ padding: 2, marginBottom: 4 }}
+          >
+            <Typography className={"animate delay-2"} variant="h4" gutterBottom>
               Skills
             </Typography>
-            <Typography variant="body1" paragraph>
+            <Typography className={"animate delay-3"} variant="body1" paragraph>
               Languages: JavaScript (Node.js), TypeScript, Python, Java, Ruby on
               Rails, C
             </Typography>
-            <Typography variant="body1" paragraph>
+            <Typography className={"animate delay-4"} variant="body1" paragraph>
               Frameworks & Libraries: React, Victory, Redux, Formik, Material UI
             </Typography>
-            <Typography variant="body1" paragraph>
+            <Typography className={"animate delay-5"} variant="body1" paragraph>
               Tools & Technologies: Git, Firebase, Google Cloud Platform, Vite,
               Mapbox, GPT-3.5 API
             </Typography>
-            <Typography variant="body1" paragraph>
+            <Typography className={"animate delay-6"} variant="body1" paragraph>
               Other: CLI Unix Commands, Assembly Language (LC3), Markdown, .env
               Configuration,
             </Typography>
